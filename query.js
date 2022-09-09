ab09d09c1722cac3e33ce2db676a70e096dbbe12
Alemayehu mekonen <itsalextwod@gmail.com>
2022-09-09 09:26:59 +0300

    responsive layout

---
+++ query.js
@@ -0,0 +1,14 @@
+$(function() {
+
+$("#roots > *").css("border", "1px solid")
+$(".article > *").css("border", "1px solid")
+$(".aside > *").css("border", "1px solid")
+$(".aside > *").width(450)
+$(".aside > *").height(300)
+$(".article > *").width(600)
+$(".article > *").height(60)
+$(".article > footer").height(40)
+$(".article > *").css("margin", ".5em")
+$(".article > *").css("borderRadius", '10px')
+
+});
